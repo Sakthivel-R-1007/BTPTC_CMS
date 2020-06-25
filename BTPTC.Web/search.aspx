234a23ea9b2ad5ac1c850870b091a0985170f138
Sakthi R <Sakthivel.R@cloud-nexus.net>
2020-06-25 19:58:14 +0530

    changes

---
+++ BTPTC.Web/search.aspx
@@ -0,0 +1,108 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="search.aspx.cs" Inherits="BTPTC.Web.search" %>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>Bishan-Toa Payoh Town Council-Search</title>
+    <!--For FB share-->
+    <meta property="og:url" content="http://btptc.medialabsstreaming.com/join-us.html" />
+    <meta property="og:title" content="Join Us | Bishan-Toa Payoh Town Council" />
+    <meta property="og:description" content="" />
+    <meta property="og:image" content="http://btptc.medialabsstreaming.com/images/join-us/banner-img.jpg" />
+   
+    <link href="Contents/css/reset.css" rel="stylesheet" />
+    <link href="Contents/css/layout.css" rel="stylesheet" />
+    <link href="Contents/css/common.css" rel="stylesheet" />
+    <link href="Contents/css/styles.css" rel="stylesheet" />
+    <link href="Contents/css/responsive.css" rel="stylesheet" />
+
+    <!--[if lt IE 9]>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<![endif]-->
+    <!--[if !(IE)|(gte IE 9)]><!-->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <!--<![endif]-->
+    <script src="Contents/scripts/jquery-ui.min.js"></script>
+    
+<%--    <script src="scripts/vendor/modernizr-2.7.1.min.js"></script>--%>
+   
+     <script src="Contents/scripts/common.js"></script>
+    <script src="Contents/Admin/js/classie.min.js"></script>
+    <script src="Contents/Admin/js/jquery.parallax-1.1.3.js"></script>
+    <script src="Contents/Admin/js/jquery.scrollTo-1.4.3.1-min.js"></script>
+    <script src="Contents/Admin/js/jquery.localscroll-1.2.7-min.js"></script>
+    <script src="Contents/Admin/js/bootstrap/bootstrap.js"></script>
+
+    <!--Facebook-->
+    <script src="//connect.facebook.net/en_US/all.js" type="text/javascript"></script>
+    <div id="fb-root"></div>
+    <script>(function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=506027563414721";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+</head>
+<body class="CONTACTUS">
+    <div class="wrapper">
+        <header>
+            <div class="header"></div>
+        </header>
+        <!--End Header-->
+
+        <div class="mainContents">
+            <div class="contactDetails desktopOnly"></div>
+            <!--End Contact Details (Contact Num, Font size adjustment and Social Media)-->
+            <div class="contactDetails mobileOnly"></div>
+            <!--End Contact Details (Contact Numbers)-->
+
+            <div class="searchResult">
+                <div class="pageContents">
+                    <div class="container">
+                        <div class="topBar">
+                            <div class="breadcrumb"><a href="index.html">Home</a><span></span>Search Result(s)</div>
+                        </div>
+                        <div class="searchResultContainer">
+                            <h1>Search Result(s)</h1>
+
+                            <p class="searchIcon"><strong><label id="countId"></label></strong> item(s) found on "<strong><%=srchSite!=null ?srchSite.SearchWords: "-" %></strong>"</p>
+
+                            <ul>
+                                <asp:PlaceHolder ID = "PlaceHolder1" runat="server" />
+                                
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--End Search Result-->
+        </div>
+        <!-- End Main Contents -->
+
+        <div class="footer"></div>
+    </div>
+    <!--End Wrapper-->
+    <script>
+
+        $('header').load('includes/header.html');
+        $('.contactDetails').load('includes/contactDetails.html');
+        $('.footer').load('includes/footer.html');
+
+        $(document).ready(function () {
+            $("#countId").html($('ul li').length)
+
+           
+        });
+    </Script>
+</body>
+</html>
+
---
+++ BTPTC.Web/search.aspx.cs
@@ -0,0 +1,131 @@
+﻿using Searchs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BTPTC.Web
+{
+    public class SearchResult
+    {
+        public string Path { get; set; }
+        public string Title { get; set; }
+    }
+    public partial class search : System.Web.UI.Page
+    {
+        protected Searchs.UserSearch srchSite;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["q"] != null)
+            {
+                UserSearch sSite = SearchSite(Convert.ToString(Request.QueryString["q"]));
+            }
+
+        }
+
+        protected Searchs.UserSearch SearchSite(string strSearch)
+        {
+
+            srchSite = new Searchs.UserSearch();
+            //Read in all the search words into one variable
+            //srchSite.RawSearchWords = srchSite.SearchWords = strSearch;
+            srchSite.SearchWords = strSearch;
+            srchSite.SearchCriteria = Searchs.SearchCriteria.Phrase;
+
+            //if (Phrase.Checked)
+            //    srchSite.SearchCriteria = Searchs.SearchCriteria.Phrase;
+            //else
+            //    if (AllWords.Checked)
+            //        srchSite.SearchCriteria = Searchs.SearchCriteria.AllWords;
+            //    else
+            //        if (AnyWords.Checked)
+            //            srchSite.SearchCriteria = Searchs.SearchCriteria.AnyWords;
+
+            string searchDir = Server.MapPath("~/");
+
+            srchSite.Search(searchDir);   // Search from full site
+            DataTable srch = srchSite.PageDataset.Tables[0];
+
+
+            StringBuilder html = new StringBuilder();
+            if (srch.Rows != null)
+            {
+                foreach (DataRow row in srch.Rows)
+                {
+                    Dictionary<string, string> routes = new Dictionary<string, string>
+                        {
+                //About US
+                            {"btptc-intro.html","Bishan-Toa Payoh Town Council"},
+                            {"our-history.html","Our History"},
+                            {"roles-responsibilities.html","Roles & Responsibilities"},
+                            {"our-mps.html","Our MPs"},
+                            {"personal-data-protection-policy.html","Personal Data Protection Policy"},
+               //Our Services      
+                            {"service-conservancy-charges.html","Service & Conservancy Charges"},
+                            {"bulky-item-removal-services.aspx","Bulky Item Removal Services"},
+                            {"booking-rates.html","Common Area & Facilities Booking Rates"},
+                            {"handyman-services.html","Handyman Services"},
+                            {"modes-of-payment.html","Modes of Payment"},
+               //Our Town 
+                            {"our-facilities.html","Our Facilities"},
+                            {"town-improvement-project.html","Town Improvement Projects"},
+                            {"our-events.html","Our Events"},
+               //Newsroom 
+                            {"newsletter.html","Newsletter"},
+                            {"media-release.html","Media Release"},
+                            {"annual-report.html","Annual Report"},
+                            {"tender.html","Tenders"},
+                            {"investment-fund.html","Investment of Town Council's Fund"},
+                            {"latest-happenings.html","Latest Happenings"},
+               //Gallery 
+                            {"gallery.html","Gallery"},
+                            {"gallery-2019-tree-planting.html","2019 Tree Planting"},
+                            {"gallery-2019-market-visit-and-pal-family-carnival.html","2019 Market Visit And Pal Family Carnival"},
+                            {"gallery-2019-shunfu-mart.html","2019 Shunfu Mart"},
+                            {"gallery-2019-thomson-sin-ming-celebration.html","2019 Thomson Sin Ming Celebration"},
+                            {"gallery-2019-tp-central-r-r-colour-scheme.html","2019 TP Central RR Colour Scheme"},
+                            {"gallery-2019-market-visit-toa-payoh.html","2019 Market Visit Toa Payoh"},
+                            {"gallery-2018-tree-planting.html","2018 Tree Planting"},
+                            {"gallery-2018-tpc-nrp-exhibition.html","2018 TPC NRP Exhibition"},
+                            {"gallery-2018-market-visit-bishan.html","2018 Market Visit Bishan"},
+                            {"gallery-2018-bn-nrp-exhibition.html","2018 BN NRP Exhibition"},
+                            {"gallery-2018-market-blk-93.html","2018 Market BLK 93"},
+               //Contact Us 
+                            {"our-offices.html","Gallery"},
+                            {"sms-alert.aspx","SMS Alert"},
+                            {"feedback.aspx","Feedback"},
+                            {"faqs.html","FAQs"},
+                            {"join-us.html","Join Us"},
+                            {"mobile-app.html","Mobile App"},
+
+                            {"index.html","Home"}
+                        };
+
+                    try
+                    {
+                        var data = routes.Where(s => !string.IsNullOrEmpty(s.Key) && s.Key.ToLower().Contains(row[3].ToString().ToLower())).FirstOrDefault();
+
+                        if (!string.IsNullOrEmpty(data.Key) && !string.IsNullOrEmpty(data.Value))
+                        {
+                            html.Append("<li>");
+                            html.Append("<p><strong>" + data.Value + "</strong ></p><p><a href='" + data.Key + "' class='uploadBtn'>Visit Page</a></p>");
+                            html.Append("</li>");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+
+                }
+            }
+            PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+            // srchSite.Search(Server.MapPath("~/pdf"));  // Search from pdf folder
+            return srchSite;
+        }
+    }
+}
